26735191a7e46971554c1047214d08a671b2f0f6
Thomas Morrison <morristr@colorado.edu>
2016-07-28 23:26:32 -0400

    Update to two secondary html pages

---
+++ listingDataBase.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE listing_DB;
+
+CREATE TABLE table_name
+(
+service varchar(255),
+price decimal(255),
+rating integer(255),
+url varchar(255),
+company varchar(255)
+);
--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 
 
 //prompt("What is your name?");
+
+
+//jquery stuff
+//$(document).ready(function('div'){
+//    .slideDown('slow')
+
+//});
+
+// java function outline
+var main = function() {
+	// Select a class with jquery (login menu)
+	$('.icon-menu').click(function() {
+        $('.menu').animate({
+            left: '0px'
+        }, 200);
+	    $('body').animate({
+    	    left: '285px'
+        }, 200);
+
+	});
+};
+    
+
+
+// jquery to run the function as soon as the HTML document has loaded
+$(document).ready(main);
